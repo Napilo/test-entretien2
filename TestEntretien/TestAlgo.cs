c52a8779097d35710c4d10ae43fbaecf7db3ddef
Citeo <63113663+Citeo@users.noreply.github.com>
2022-04-11 14:48:09 +0200

    Update TestAlgo.cs

--- TestEntretien/TestAlgo.cs
+++ TestEntretien/TestAlgo.cs
@@ -13,31 +13,26 @@ namespace TestEntretien
         /// grouper les anagrammes 
         /// 10 min
         /// </summary>
-        public static void RemoveDuplicate()
-        {
-            Console.WriteLine("RemoveDuplicate start");
+        public static void Test1()
+        {            
             List<List<int>> datas = new List<List<int>>()
             {
                 new List<int>() { 1,2,3,4,5,6,7,8,9},
                 new List<int>() { 6,10,11,8,13,14,15,23},
                 new List<int>() { 6,23,7,8},
                 new List<int>() { 2,50,100,88},
-            };
-            
-            
-            
+            };             
         }
         
-        public static void GroupAnagram()
-        {
-            Console.WriteLine("GroupAnagram start");
-            string[] input = { "reza","eat", "tea", "tan", "ate", "nat", "bat", "bta", "azer" };
-
-           
+        public static void Test2()
+        {            
+            var input = { "reza","eat", "tea", "tan", "ate", "nat", "bat", "bta", "azer" };           
         }
-
-       
-
-
+        
+        public void Test3()
+        {
+            var input = new [] {1, 2, 7, 8, 11, 7};
+            var sum = 20;
+        }
     }
 }
